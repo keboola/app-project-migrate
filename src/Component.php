98985613854f4c6452681ada1544983893161fe4
Martin Halamíček <martin@halamicek.cz>
2018-05-22 13:43:30 +0200

    basic app flow

--- src/Component.php
+++ src/Component.php
@@ -5,12 +5,43 @@ declare(strict_types=1);
 namespace Keboola\AppProjectMigrate;
 
 use Keboola\Component\BaseComponent;
+use Keboola\Component\UserException;
+use Keboola\StorageApi\Client as StorageClient;
+use Keboola\StorageApi\ClientException as StorageClientException;
 
 class Component extends BaseComponent
 {
     public function run(): void
     {
-        // @TODO implement
+        /** @var Config $config */
+        $config = $this->getConfig();
+
+        $sourceProjectClient = $this->createStorageClient([
+            'url' => $config->getSourceProjectUrl(),
+            'token' => $config->getSourceProjectToken(),
+        ]);
+        try {
+            $sourceTokenInfo = $sourceProjectClient->verifyToken();
+        } catch (StorageClientException $e) {
+            throw new UserException('Cannot authorize source project: ' . $e->getMessage(), $e->getCode(), $e);
+        }
+
+        $destProjectClient = $this->createStorageClient([
+            'url' => getenv('KBC_URL'),
+            'token' => getenv('KBC_TOKEN'),
+        ]);
+
+        $migrate = new Migrate(
+            Utils::createDockerRunnerClientFromStorageClient($sourceProjectClient),
+            Utils::createDockerRunnerClientFromStorageClient($destProjectClient)
+        );
+    }
+
+    private function createStorageClient(array $params): StorageClient
+    {
+        $client = new StorageClient($params);
+        $client->setRunId($this->getKbcRunId());
+        return $client;
     }
 
     protected function getConfigClass(): string
@@ -21,5 +52,10 @@ class Component extends BaseComponent
     protected function getConfigDefinitionClass(): string
     {
         return ConfigDefinition::class;
+    }
+
+    private function getKbcRunId(): string
+    {
+        return (string) getenv('KBC_RUNID');
     }
 }
---
+++ src/Utils.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\AppProjectMigrate;
+
+use Keboola\StorageApi\Client as StorageClient;
+use Keboola\Syrup\Client as SyrupClient;
+
+class Utils
+{
+    private const DOCKER_RUNNER_SERVICE_ID = 'docker-runner';
+
+    public static function getKeboolaServiceUrl(array $services, string $serviceId): string
+    {
+        $foundServices = array_values(array_filter($services, function ($service) use ($serviceId) {
+            return $service['id'] === $serviceId;
+        }));
+        if (empty($foundServices)) {
+            throw new \Exception('syrup service not found');
+        }
+        return $foundServices[0]['url'];
+    }
+
+    public static function createDockerRunnerClientFromStorageClient(StorageClient $sapiClient)
+    {
+        $baseUrl = self::getKeboolaServiceUrl(
+            $sapiClient->indexAction()['services'],
+            self::DOCKER_RUNNER_SERVICE_ID
+        );
+        return new SyrupClient([
+            'url' => $baseUrl,
+            'token' => $sapiClient->getTokenString(),
+            'super' => 'docker',
+            'runId' => $sapiClient->getRunId(),
+        ]);
+    }
+}
