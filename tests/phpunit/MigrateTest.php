18409e917b310662ee5315a1dfb3ce608c8c6687
Ondra Jodas <ondrej.jodas@keboola.com>
2020-10-05 12:54:21 +0200

    fix phpcs

--- tests/phpunit/MigrateTest.php
+++ tests/phpunit/MigrateTest.php
@@ -127,7 +127,6 @@ class MigrateTest extends TestCase
                 'status' => 'success',
             ]);
 
-
         $migrate = new Migrate(
             $sourceClientMock,
             $destClientMock,
