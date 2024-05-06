7c3a7995fc3f5f3e303d73b69ce8ef456d03cdfb
Roman Pištěk <roman.pistek@keboola.com>
2024-05-06 12:11:31 +0200

    Update project envs in GA workflow, fix `invalid-token-permissions` test

--- tests/functional/DatadirTest.php
+++ tests/functional/DatadirTest.php
@@ -8,5 +8,11 @@ use Keboola\DatadirTests\DatadirTestCase;
 
 class DatadirTest extends DatadirTestCase
 {
+    protected function setUp(): void
+    {
+        parent::setUp();
 
+        $nonMasterDestToken = getenv('KBC_TOKEN_NOT_MASTER');
+        putenv('KBC_TOKEN=' . $nonMasterDestToken);
+    }
 }
